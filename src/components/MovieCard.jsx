c1cccbabb358153bdab6b2328b7cc7c889478637
Irasha-Senarathna <ira.sena715@gmail.com>
2026-02-10 11:45:40 +0530

    develop movie card compomnent

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React from 'react'
 
-const IMAGE_BASE = 'https://image.tmdb.org/t/p/w300';
+const MovieCard = ({ movie:
+  { title, vote_average, poster_path, release_date, original_language }
+}) => {
+  return (
+    <div className="movie-card">
+      <img
+        src={poster_path ?
+          `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'}
+        alt={title}
+      />
 
-export default function MovieCard({ movie }) {
-    const poster = movie?.poster_path ? `${IMAGE_BASE}${movie.poster_path}` : './poster-placeholder.png';
-    const title = movie?.title || movie?.name || 'Untitled';
-    return (
-        <li className="movie-card">
-            <img src={poster} alt={title} />
-            <h3>{title}</h3>
-        </li>
-    );
+      <div className="mt-4">
+        <h3>{title}</h3>
+
+        <div className="content">
+          <div className="rating">
+            <img src="star.svg" alt="Star Icon" />
+            <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+          </div>
+
+          <span>•</span>
+          <p className="lang">{original_language}</p>
+
+          <span>•</span>
+          <p className="year">
+            {release_date ? release_date.split('-')[0] : 'N/A'}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
+export default MovieCard
